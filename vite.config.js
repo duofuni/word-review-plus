5e952e70d479106d1761729ba7398f3d5b52f6ac
wm <1920754788@qq.com>
2026-03-19 17:22:01 +0800

    update1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/word-review-plus/',
+  build: {
+    outDir: 'docs'
+  },
 })
